68ff5a7369ca6ec5e7ec85d5094a28e206aa7e7e
Vinoth Kannan <svkn.87@gmail.com>
2020-05-17 22:07:08 +0530

    FEATURE: add pomodoro timer functionality.
    And add system tray icon.

--- app/index.js
+++ app/index.js
@@ -1,29 +1,7 @@
 const { ipcRenderer } = require("electron");
-const moment = require("moment");
 require("dotenv").config();
-let duration = 65;
-let format, timer;
 
-const tick = () => {
-  duration = duration - 1;
-
-  if (duration >= 3600) {
-    format = "H:m:s";
-  } else if (duration >= 60) {
-    format = "m:s";
-  } else {
-    format = "s";
-  }
-
-  const time = moment({}).startOf("day").seconds(duration).format(format);
-  timerDiv.innerHTML = time;
-
-  if (duration <= 0) {
-    clearInterval(timer);
-  }
-};
-
-ipcRenderer.on("init", () => {
+ipcRenderer.on("load", () => {
   const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -36,10 +14,7 @@ ipcRenderer.on("init", () => {
   firebase.initializeApp(firebaseConfig);
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      userDiv.innerHTML = user.email;
-      duration = process.env.DURATION_SECONDS;
-      tick();
-      timer = setInterval(tick, 1000);
+      ipcRenderer.send("loggedIn");
     } else {
       const ui = new firebaseui.auth.AuthUI(firebase.auth());
       ui.start("#firebaseAuthDiv", {
--- main.js
+++ main.js
@@ -1,24 +1,81 @@
-const { app, BrowserWindow } = require("electron");
+const { app, ipcMain, Tray, Menu, BrowserWindow } = require("electron");
+let mainWindow;
+let canCloseWindows = false;
 
-function createWindow() {
-  let win = new BrowserWindow({
-    frame: false,
-    width: 800,
-    height: 600,
+function createWindow(file, opts = {}) {
+  const win = new BrowserWindow(opts);
+  // win.webContents.openDevTools();
+  win.loadURL(`file://${__dirname}/app/${file}.html`);
+
+  win.webContents.on("did-finish-load", () => {
+    win.webContents.send("load");
+  });
+
+  win.on("close", (e) => {
+    if (!canCloseWindows) e.preventDefault();
+  });
+
+  return win;
+}
+
+function createMainWindow() {
+  mainWindow = createWindow("index", {
+    show: false,
     webPreferences: {
       nodeIntegration: true,
       nativeWindowOpen: true
     }
   });
-  win.webContents.openDevTools();
-  win.loadURL(`file://${__dirname}/app/index.html`);
 
-  win.webContents.on("did-finish-load", () => {
-    win.webContents.send("init");
+  mainWindow.on("close", (e) => {
+    if (!canCloseWindows) e.preventDefault();
+    mainWindow.minimize();
+    mainWindow.hide();
+  });
+
+  ipcMain.on("loggedIn", (user) => {
+    const win = createPomodoroWindow();
+  });
+
+  tray = new Tray("images/vinkas.png");
+  tray.on("click", () => {
+    mainWindow.show();
+  });
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Quit",
+      click() {
+        canCloseWindows = true;
+        BrowserWindow.getAllWindows().forEach((window) => {
+          window.close();
+        });
+        app.quit();
+      }
+    }
+  ]);
+  tray.setContextMenu(contextMenu);
+}
+
+function createPomodoroWindow() {
+  const win = createWindow("pomodoro", {
+    frame: false,
+    width: 300,
+    height: 100,
+    alwaysOnTop: true,
+    minimizable: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  ipcMain.on("takeBreak", () => {
+    win.maximize();
+    win.show();
   });
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createMainWindow);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
@@ -28,6 +85,6 @@ app.on("window-all-closed", () => {
 
 app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createMainWindow();
   }
 });
